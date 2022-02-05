7d66d1cf6ad6b85f2cfb269ec4c606721c194e13
thdgorhkd97 <thdgorhkd97@naver.com>
2022-02-06 00:44:15 +0900

    0206

---
+++ ClearServer/src/main/java/com/umc/clearserver/src/friend/FriendController.java
@@ -0,0 +1,81 @@
+package com.umc.clearserver.src.friend;
+
+import com.umc.clearserver.src.config.BaseException;
+import com.umc.clearserver.src.config.BaseResponse;
+import com.umc.clearserver.src.user.model.GetUserRes;
+import com.umc.clearserver.src.user.model.PostSignUpReq;
+import com.umc.clearserver.src.user.model.PostSignUpRes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import com.umc.clearserver.src.config.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import com.umc.clearserver.src.friend.model.*;
+import java.util.List;
+
+import static com.umc.clearserver.src.config.BaseResponseStatus.*;
+import static com.umc.clearserver.src.utils.ValidationRegex.isRegexEmail;
+
+@RequestMapping("/friends")
+@RestController
+public class FriendController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass()); // Log를 남기기: 일단은 모르고 넘어가셔도 무방합니다.
+
+
+    @Autowired
+    private final FriendProvider friendProvider;
+    @Autowired
+    private final FriendService friendService;
+
+    public FriendController(FriendProvider friendProvider, FriendService friendService) {
+        this.friendProvider = friendProvider;
+        this.friendService = friendService;
+    }
+
+    @ResponseBody   // return되는 자바 객체를 JSON으로 바꿔서 HTTP body에 담는 어노테이션.
+    //  JSON은 HTTP 통신 시, 데이터를 주고받을 때 많이 쓰이는 데이터 포맷.
+    @GetMapping("") // (GET) http://localhost:23628/friend
+    // GET 방식의 요청을 매핑하기 위한 어노테이션
+    public BaseResponse<List<GetUserRes>> getUsers(@RequestParam(required = false) String email) {
+        //  @RequestParam은, 1개의 HTTP Request 파라미터를 받을 수 있는 어노테이션(?뒤의 값). default로 RequestParam은 반드시 값이 존재해야 하도록 설정되어 있지만, (전송 안되면 400 Error 유발)
+        //  지금 예시와 같이 required 설정으로 필수 값에서 제외 시킬 수 있음
+        //  defaultValue를 통해, 기본값(파라미터가 없는 경우, 해당 파라미터의 기본값 설정)을 지정할 수 있음
+        try {
+            if (email == null) { // query string인 email 없을 경우, 그냥 전체 유저정보를 불러온다.
+                List<GetUserRes> getUsersRes = friendProvider.getUsers();
+                System.out.println(" email을 입력해 주시기 바랍니다.");
+                return new BaseResponse<>(getUsersRes);
+            }
+            // query string인 email이 있을 경우, 조건을 만족하는 유저정보들을 불러온다.
+            List<GetUserRes> getUsersRes = friendProvider.getUsersByEmail(email);
+            return new BaseResponse<>(getUsersRes);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    /**
+     * 유저 비활성화 API
+     * [PATCH] /friends/delete/{friend.id}
+     */
+    @ResponseBody
+    @PatchMapping("/delete")
+    @Transactional
+    public BaseResponse<String> deleteFriend(@RequestBody friend friend) {
+        try {
+            PatchFriendReq patchFriendReq = new PatchFriendReq(friend.getUser1(), friend.getUser2());
+            friendService.deleteFriend(patchFriendReq);
+
+            String result = "친구 삭제 성공";
+            return new BaseResponse<>(result);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+}
---
+++ ClearServer/src/main/java/com/umc/clearserver/src/friend/FriendDao.java
@@ -0,0 +1,75 @@
+package com.umc.clearserver.src.friend;
+
+import com.umc.clearserver.src.user.model.GetUserRes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import com.umc.clearserver.src.friend.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Repository
+public class FriendDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired //readme 참고
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public int checkEmail(String email) {
+        String checkEmailQuery = "select exists(select email from user where email = ?)"; // User Table에 해당 email 값을 갖는 유저 정보가 존재하는가?
+        String checkEmailParams = email; // 해당(확인할) 이메일 값
+        return this.jdbcTemplate.queryForObject(checkEmailQuery,
+                int.class,
+                checkEmailParams); // checkEmailQuery, checkEmailParams를 통해 가져온 값(intgud)을 반환한다. -> 쿼리문의 결과(존재하지 않음(False,0),존재함(True, 1))를 int형(0,1)으로 반환됩니다.
+    }
+
+    public List<GetUserRes> getUsers() {
+        String getUsersQuery = "select * from user"; //User 테이블에 존재하는 모든 회원들의 정보를 조회하는 쿼리
+        return this.jdbcTemplate.query(getUsersQuery,
+                (rs, rowNum) -> new GetUserRes(
+                        rs.getInt("id"),
+                        rs.getString("nickname"),
+                        rs.getString("email"),
+                        rs.getString("password")) // RowMapper(위의 링크 참조): 원하는 결과값 형태로 받기
+        ); // 복수개의 회원정보들을 얻기 위해 jdbcTemplate 함수(Query, 객체 매핑 정보)의 결과 반환(동적쿼리가 아니므로 Parmas부분이 없음)
+    }
+
+    public List<GetUserRes> getUsersByEmail(String email) {
+        String getUsersByNicknameQuery = "select * from user where email =?"; // 해당 이메일을 만족하는 유저를 조회하는 쿼리문
+        String getUsersByNicknameParams = email;
+        return this.jdbcTemplate.query(getUsersByNicknameQuery,
+                (rs, rowNum) -> new GetUserRes(
+                        rs.getInt("id"),
+                        rs.getString("nickname"),
+                        rs.getString("email"),
+                        rs.getString("password")), // RowMapper(위의 링크 참조): 원하는 결과값 형태로 받기
+                getUsersByNicknameParams); // 해당 닉네임을 갖는 모든 User 정보를 얻기 위해 jdbcTemplate 함수(Query, 객체 매핑 정보, Params)의 결과 반환
+    }
+
+    public int deleteFriend(PatchFriendReq patchFriendReq) {
+        /*
+        String deleteProductQuery = "update Friend set state = ? where user1 = ? and user2 = ? "; // 해당 productIdx를 만족하는 Product를 해당 productName으로 변경한다.
+        Object[] deleteProductParams = new Object[]{"inactive", patchFriendReq.getUser1(), patchFriendReq.getUser2()}; // 주입될 값들(nickname, userIdx) 순
+
+        return this.jdbcTemplate.update(deleteProductQuery, deleteProductParams);
+        */
+        int u1 = patchFriendReq.getUser1();
+        int u2 = patchFriendReq.getUser2();
+        int friendId = jdbcTemplate.queryForObject(
+                "SELECT id from friend where user1 = ? and user2 = ?", Integer.class, u1, u2);
+        String deleteFriendQuery = "update friend set state = ? where id = ?"; // 해당 id를 만족하는 Friend를 해당 state으로 변경한다.
+        Object[] deleteFriendParams = new Object[]{0, friendId}; // 주입될 값들(state, id) 순
+
+        return this.jdbcTemplate.update(deleteFriendQuery, deleteFriendParams); // 대응시켜 매핑시켜 쿼리 요청(생성했으면 1, 실패했으면 0)
+
+    }
+}
