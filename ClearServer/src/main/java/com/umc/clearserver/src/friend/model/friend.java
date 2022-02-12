7b2ddc6cb486eb70e6df28c69b36284458077b27
haochaen73 <kyjjulia07@gmail.com>
2022-02-12 14:58:37 +0900

    Fix friend model
    찐 마지막 friend

--- ClearServer/src/main/java/com/umc/clearserver/src/friend/model/friend.java
+++ ClearServer/src/main/java/com/umc/clearserver/src/friend/model/friend.java
@@ -1,13 +1,11 @@
 package com.umc.clearserver.src.friend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter // 해당 클래스에 대한 접근자 생성
 @Setter // 해당 클래스에 대한 설정자 생성
 @AllArgsConstructor // 해당 클래스의 모든 멤버 변수(userIdx, nickname, email, password)를 받는 생성자를 생성
-
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class friend {
     private int id;
     private int user1;
